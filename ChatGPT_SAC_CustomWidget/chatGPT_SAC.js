90fef91e5f637ebde23fb83c62275e7eee33e5be
Olumide Johnson <57791906+ojohnsonolanrewaju@users.noreply.github.com>
2023-11-11 12:33:27 +0100

    Update chatGPT_SAC.js

--- ChatGPT_SAC_CustomWidget/chatGPT_SAC.js
+++ ChatGPT_SAC_CustomWidget/chatGPT_SAC.js
@@ -1,49 +1,128 @@
-var ajaxCall = (key, url, prompt) => {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: url,
-      type: "POST",
-      dataType: "json",
-      data: JSON.stringify({
-        model: "text-davinci-002",
-        prompt: prompt,
-        max_tokens: 1024,
-        n: 1,
-        temperature: 0.5,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${key}`,
-      },
-      crossDomain: true,
-      success: function (response, status, xhr) {
-        resolve({ response, status, xhr });
-      },
-      error: function (xhr, status, error) {
-        const err = new Error("xhr error");
-        err.status = xhr.status;
-        reject(err);
-      },
-    });
-  });
-};
-
-const url = "https://api.openai.com/v1";
-
 (function () {
-  const template = document.createElement("template");
+  let template = document.createElement("template");
   template.innerHTML = `
-        <style>
-        </style>
-        <div id="root" style="width: 100%; height: 100%;">
-        </div>
-      `;
-  class MainWebComponent extends HTMLElement {
-    async post(apiKey, endpoint, prompt) {
-      const { response } = await ajaxCall(apiKey, `${url}/${endpoint}`, prompt);
-      //console.log(response.choices[0].text);
-      return response.choices[0].text;
+      <style>
+        :host {}
+  
+  /* Style for the container */
+  div {
+    margin: 50px auto;
+    max-width: 600px;
+  }
+  
+  /* Style for the input container */
+  .input-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  
+  /* Style for the input field */
+  #prompt-input {
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    width: 70%;
+  }
+  
+  /* Style for the button */
+  #generate-button {
+    padding: 10px;
+    font-size: 16px;
+    background-color: #3cb6a9;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 25%;
+  }
+  
+  /* Style for the generated text area */
+  #generated-text {
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  width:96%;
+  }
+      </style>
+     <div>
+  <center>
+  <img src="https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Emblem.png" width="200"/>
+  <h1>ChatGPT</h1></center>
+    <div class="input-container">
+      <input type="text" id="prompt-input" placeholder="Enter a prompt">
+      <button id="generate-button">Generate Text</button>
+    </div>
+    <textarea id="generated-text" rows="10" cols="50" readonly></ textarea>
+  </div>
+    `;
+  class Widget extends HTMLElement {
+    constructor() {
+      super();
+      let shadowRoot = this.attachShadow({
+        mode: "open"
+      });
+      shadowRoot.appendChild(template.content.cloneNode(true));
+      this._props = {};
+    }
+    async connectedCallback() {
+      this.initMain();
+    }
+    async initMain() {
+      const generatedText = this.shadowRoot.getElementById("generated-text");
+      generatedText.value = "";
+      const {
+        apiKey
+      } = this._props || "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";
+      const {
+        max_tokens
+      } = this._props || 1024;
+      const generateButton = this.shadowRoot.getElementById("generate-button");
+      generateButton.addEventListener("click", async () => {
+        const promptInput = this.shadowRoot.getElementById("prompt-input");
+        const generatedText = this.shadowRoot.getElementById("generated-text");
+        generatedText.value = "Finding result...";
+        const prompt = promptInput.value;
+        const response = await fetch("https://api.openai.com/v1/completions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + apiKey
+          },
+          body: JSON.stringify({
+            "model": "text-davinci-002",
+            "prompt": prompt,
+            "max_tokens": parseInt(max_tokens),
+            "n": 1,
+            "temperature": 0.5
+          })
+        });
+
+        if (response.status === 200) {
+          const {
+            choices
+          } = await response.json();
+          const generatedTextValue = choices[0].text;
+          generatedText.value = generatedTextValue.replace(/^\n+/, '');
+        } else {
+          const error = await response.json();
+          alert("OpenAI Response: " + error.error.message);
+          generatedText.value = "";
+        }
+      });
+    }
+    onCustomWidgetBeforeUpdate(changedProperties) {
+      this._props = {
+        ...this._props,
+        ...changedProperties
+      };
+    }
+    onCustomWidgetAfterUpdate(changedProperties) {
+      this.initMain();
     }
   }
-  customElements.define("custom-widget", MainWebComponent);
+  customElements.define("com.Johnson.sap.sac.genAI", Widget);
 })();
